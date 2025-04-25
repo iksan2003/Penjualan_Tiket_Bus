e2707c764f174887321429e1dbf8de9031f33b90
iksan2003 <ichsanm621@gmail.com>
2025-04-25 14:38:41 +0700

    Delete akun.py

--- akun.py
+++
@@ -1,6 +0,0 @@
-class AkunPUBG:
-    def __init__(self, username, level, harga, detail):
-        self.username = username
-        self.level = level
-        self.harga = harga
-        self.detail = detail
